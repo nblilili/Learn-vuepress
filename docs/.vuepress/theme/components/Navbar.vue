d457e87213e7441001be4fb1aabe364f98946dd5
jiashun.mao <jiashun.mao@juphoon.com.cn>
2020-08-25 15:19:48 +0800

    测试语言

--- docs/.vuepress/theme/components/Navbar.vue
+++ docs/.vuepress/theme/components/Navbar.vue
@@ -90,6 +90,7 @@
 <script>
 import axios from "axios";
 import AlgoliaSearchBox from "@AlgoliaSearchBox";
+import { resolveNavLinkItem } from "../util";
 export default {
   name: "NavBar",
   components: {
@@ -122,8 +123,51 @@ export default {
     isAlgoliaSearch() {
       return this.algolia && this.algolia.apiKey && this.algolia.indexName;
     },
+    userNav() {
+      return this.$themeLocaleConfig.nav || this.$site.themeConfig.nav || [];
+    },
+    nav() {
+      const { locales } = this.$site;
+      if (locales && Object.keys(locales).length > 1) {
+        const currentLink = this.$page.path;
+        const routes = this.$router.options.routes;
+        const themeLocales = this.$site.themeConfig.locales || {};
+        const languageDropdown = {
+          text: this.$themeLocaleConfig.selectText || "Languages",
+          ariaLabel: this.$themeLocaleConfig.ariaLabel || "Select language",
+          items: Object.keys(locales).map((path) => {
+            const locale = locales[path];
+            const text =
+              (themeLocales[path] && themeLocales[path].label) || locale.lang;
+            let link;
+            // Stay on the current page
+            if (locale.lang === this.$lang) {
+              link = currentLink;
+            } else {
+              // Try to stay on the same page
+              link = currentLink.replace(this.$localeConfig.path, path);
+              // fallback to homepage
+              if (!routes.some((route) => route.path === link)) {
+                link = path;
+              }
+            }
+            return { text, link };
+          }),
+        };
+        return [...this.userNav, languageDropdown];
+      }
+      return this.userNav;
+    },
+    userLinks() {
+      return (this.nav || []).map((link) => {
+        return Object.assign(resolveNavLinkItem(link), {
+          items: (link.items || []).map(resolveNavLinkItem),
+        });
+      });
+    },
   },
   mounted() {
+    console.log(this.userLinks)
     this.$EventBus.$on("changeNav", () => {
       this.showNav = !this.showNav;
     });
