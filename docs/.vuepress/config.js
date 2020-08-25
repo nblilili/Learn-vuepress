94ae2e3b3b54730f9c2a2f1dbf33264a2b745503
haoli.li <haoli.li@juphoon.com.cn>
2020-08-25 16:12:13 +0800

    Revert "modify nothing"
    This reverts commit 341cc3999d854aa82adff6edae42d7fefff65ba2.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,11 +5,6 @@ const navConf = require('./config/navConf.js');
 const navUtil = require('./utils/navUtil.js');
 
 module.exports = {
-  locales: {
-    '/': {},
-    '/en/': {},
-    '/cn/': {},
-  },
   plugins: [
     ['vuepress-plugin-code-copy', true]
   ],
@@ -28,64 +23,14 @@ module.exports = {
     }]
   ],
   themeConfig: {
-    locales: {
-      '/':  {
-        selectText: '选择语言',
-        label: '简体中文',
-        editLinkText: '在 GitHub 上编辑此页',
-        serviceWorker: {
-          updatePopup: {
-            message: "发现新内容可用.",
-            buttonText: "刷新"
-          }
-        },
-        // 当前 locale 的 algolia docsearch 选项
-        algolia: {
-          apiKey: '48aefdd5edf4e5a94a5ffee6f59e5271',
-          indexName: 'juphoon_developer'
-        },
-        nav: navUtil.getNavConf('cn'),
-        sidebar: sidebarUtil.getSidebarConf('docs/cn/'),
-        tagConfig: tagsUtil.getTagsConf('docs/cn/')
-      },
-      '/cn/':  {
-        selectText: '选择语言',
-        label: '简体中文',
-        editLinkText: '在 GitHub 上编辑此页',
-        serviceWorker: {
-          updatePopup: {
-            message: "发现新内容可用.",
-            buttonText: "刷新"
-          }
-        },
-        // 当前 locale 的 algolia docsearch 选项
-        algolia: {
-          apiKey: '48aefdd5edf4e5a94a5ffee6f59e5271',
-          indexName: 'juphoon_developer'
-        },
-        nav: navUtil.getNavConf('cn'),
-        sidebar: sidebarUtil.getSidebarConf('docs/cn/'),
-        tagConfig: tagsUtil.getTagsConf('docs/cn/')
-      },
-      '/en/': {
-        selectText: 'Languages',
-        label: 'English',
-        editLinkText: 'Edit this page on GitHub',
-        serviceWorker: {
-          updatePopup: {
-            message: "New content is available",
-            buttonText: "Refresh"
-          }
-        },
-        nav: navUtil.getNavConf('en'),
-        algolia: {
-          apiKey: '48aefdd5edf4e5a94a5ffee6f59e5271',
-          indexName: 'juphoon_developer'
-        },
-        sidebar: sidebarUtil.getSidebarConf('docs/en/'),
-        tagConfig: tagsUtil.getTagsConf('docs/en/')
-      }
-    }
+    algolia: {
+      apiKey: '48aefdd5edf4e5a94a5ffee6f59e5271',
+      indexName: 'juphoon_developer'
+    },
+    // nav: navUtil.getNavConf('cn'),
+    nav: navUtil.getNavConf('cn'),
+    // sidebar: sidebarConf
+    sidebar: sidebarUtil.getSidebarConf('docs/cn/'),
+    tagConfig: tagsUtil.getTagsConf('docs/cn/')
   }
-  
 }
