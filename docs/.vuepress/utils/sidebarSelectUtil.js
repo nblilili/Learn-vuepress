6b6a12df791f18d14a54abc1b249dbbb9e540d74
haoli.li <haoli.li@juphoon.com.cn>
2020-08-28 15:01:04 +0800

    update: 删除配置文件中的base 因为报错了

--- docs/.vuepress/utils/sidebarSelectUtil.js
+++ docs/.vuepress/utils/sidebarSelectUtil.js
@@ -4,34 +4,34 @@ module.exports = {
         title: "平台",
         children: [{
           title: "菊风云平台",
-          url: base + locale + "/juphoon_platform/"
+          url: locale + "/juphoon_platform/"
         }]
       },
       {
         title: "产品",
         children: [{
             title: "一对一语音通话",
-            url: base + locale + "/00_o2o_audio/"
+            url: locale + "/00_o2o_audio/"
           },
           {
             title: "一对一视频通话",
-            url: base + locale + "/01_o2o_video/"
+            url: locale + "/01_o2o_video/"
           },
           {
             title: "多方语音通话",
-            url: base + locale + "/02_mult_audio/"
+            url: locale + "/02_mult_audio/"
           },
           {
             title: "多方视频通话",
-            url: base + locale + "/03_mult_video/"
+            url: locale + "/03_mult_video/"
           },
           {
             title: "语音互动直播",
-            url: base + locale + "/04_audio_live/"
+            url: locale + "/04_audio_live/"
           },
           {
             title: "视频互动直播",
-            url: base + locale + "/05_video_live/"
+            url: locale + "/05_video_live/"
           }
         ]
       },
@@ -39,19 +39,19 @@ module.exports = {
         title: "扩展功能",
         children: [{
             title: "WebRTC",
-            url: base + locale + "/WebRTC"
+            url: locale + "/WebRTC"
           },
           {
             title: "服务器录制",
-            url: base + locale + "/服务器录制"
+            url: locale + "/服务器录制"
           },
           {
             title: "屏幕共享",
-            url: base + locale + "/屏幕共享"
+            url: locale + "/屏幕共享"
           },
           {
             title: "CDN 推流",
-            url: base + locale + "/CDN 推流"
+            url: locale + "/CDN 推流"
           }
         ]
       }
@@ -60,34 +60,34 @@ module.exports = {
       title: "platform",
       children: [{
         title: "Juphoon Platform",
-        url: base + locale + "/juphoon_platform/"
+        url: locale + "/juphoon_platform/"
       }]
     },
     {
       title: "Product",
       children: [{
           title: "One to One Voice Calling",
-          url: base + locale + "/00_o2o_audio/"
+          url: locale + "/00_o2o_audio/"
         },
         {
           title: "One to One Video Calling",
-          url: base + locale + "/01_o2o_video/"
+          url: locale + "/01_o2o_video/"
         },
         {
           title: "Group Voice Calling",
-          url: base + locale + "/02_mult_audio/"
+          url: locale + "/02_mult_audio/"
         },
         {
           title: "Group Video Calling",
-          url: base + locale + "/03_mult_video/"
+          url: locale + "/03_mult_video/"
         },
         {
           title: "Live Interactive Audio Streaming",
-          url: base + locale + "/04_audio_live/"
+          url: locale + "/04_audio_live/"
         },
         {
           title: "Live Interactive Video Streaming",
-          url: base + locale + "/05_video_live/"
+          url: locale + "/05_video_live/"
         }
       ]
     }
